571d02c44f243c6ff95e3c01d6b79bade6904e80
Nedim Spahic <spahicnedim3@gmail.com>
2024-07-24 22:28:43 +0200

    protected routes and role based access fix

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FaHome, FaBuilding, FaUser, FaUserPlus } from "react-icons/fa";
+import {
+  FaHome,
+  FaBuilding,
+  FaUser,
+  FaUserPlus,
+  FaChevronUp,
+  FaChevronDown,
+} from "react-icons/fa";
 import useAuthorization from "./useAuthorization";
 
 const Sidebar = () => {
   const isVlasnik = useAuthorization([1, 3]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   return (
-    <div className='w-64 h-screen bg-gray-800 text-white'>
+    <div className='w-60 h-screen bg-gray-800 text-white'>
       <ul>
         <li>
           <NavLink
@@ -18,35 +30,50 @@ const Sidebar = () => {
           </NavLink>
         </li>
         {isVlasnik && (
-          <>
-            <li>
-              <NavLink
-                to='dashboard/dodaj-firmu'
-                className='flex items-center p-4 hover:bg-gray-700'
-                activeClassName='bg-gray-600'
-              >
-                <FaBuilding className='mr-2' /> Create Company
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='dashboard/prava-zaposlenih'
-                className='flex items-center p-4 hover:bg-gray-700'
-                activeClassName='bg-gray-600'
-              >
-                <FaUser className='mr-2' /> Assign Employees
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='dashboard/dodaj-zaposlenog'
-                className='flex items-center p-4 hover:bg-gray-700'
-                activeClassName='bg-gray-600'
-              >
-                <FaUserPlus className='mr-2' /> Add Employee
-              </NavLink>
-            </li>
-          </>
+          <li>
+            <div
+              className='flex items-center p-4 hover:bg-gray-700 cursor-pointer'
+              onClick={toggleDropdown}
+            >
+              <FaBuilding className='mr-2' /> Dashboard
+              {isDropdownOpen ? (
+                <FaChevronUp className='ml-auto' />
+              ) : (
+                <FaChevronDown className='ml-auto' />
+              )}
+            </div>
+            {isDropdownOpen && (
+              <ul className='pl-4'>
+                <li>
+                  <NavLink
+                    to='dashboard/dodaj-firmu'
+                    className='flex items-center p-4 hover:bg-gray-700'
+                    activeClassName='bg-gray-600'
+                  >
+                    <FaBuilding className='mr-2' /> Create Company
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to='dashboard/prava-zaposlenih'
+                    className='flex items-center p-4 hover:bg-gray-700'
+                    activeClassName='bg-gray-600'
+                  >
+                    <FaUser className='mr-2' /> Assign Employees
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to='dashboard/dodaj-zaposlenog'
+                    className='flex items-center p-4 hover:bg-gray-700'
+                    activeClassName='bg-gray-600'
+                  >
+                    <FaUserPlus className='mr-2' /> Add Employee
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </li>
         )}
       </ul>
     </div>
